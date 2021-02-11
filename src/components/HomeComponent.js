e2792fe8184b209851c41843b452c4908a03e143
Atula Demon <caikinhdenlc@gmail.com>
2021-02-11 23:53:02 +0700

    Course 2 - Week 2: Single Page Applications Part 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
+
+function RenderCard({item}) {
+    return(
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
 
 function Home(prop) {
     return(
         <div className="container">
-            <h4>Home</h4>
+            <div className="row align-items-start">
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={prop.dish} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={prop.promotion} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={prop.leader} />
+                </div>
+            </div>
         </div>
     )
 }
